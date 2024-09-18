92ed7c3a7961bd9f14dcbb50977df4aa99d06def
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-18 12:01:18 +0100

    feat: add getallalbums to albumapiservice

--- app/src/main/java/com/northcoders/recordshopapp/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshopapp/service/AlbumApiService.java
@@ -1,4 +1,13 @@
 package com.northcoders.recordshopapp.service;
 
+import com.northcoders.recordshopapp.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+    @GET("records")
+    public Call<List<Album>> getAllAlbums();
 }
