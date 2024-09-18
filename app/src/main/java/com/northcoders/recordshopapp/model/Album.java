bf0df41f1c530dec6f128de5c13b6390efd713e7
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-18 11:16:47 +0100

    feat: annotate album for gson

--- app/src/main/java/com/northcoders/recordshopapp/model/Album.java
+++ app/src/main/java/com/northcoders/recordshopapp/model/Album.java
@@ -1,10 +1,17 @@
 package com.northcoders.recordshopapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Album {
+    @SerializedName("id")
     int id;
+    @SerializedName("albumName")
     String albumName;
+    @SerializedName("artist")
     String artist;
+    @SerializedName("releaseYear")
     int releaseYear;
+    @SerializedName("genre")
     String genre;
 
     public Album(int id, String albumName, String artist, int releaseYear, String genre) {
