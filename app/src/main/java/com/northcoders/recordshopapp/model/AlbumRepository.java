7cafd745f7bb42b24909d98a5b540ffd0d2c80cf
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-18 12:24:02 +0100

    feat: add getmutablelivedata method

--- app/src/main/java/com/northcoders/recordshopapp/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/recordshopapp/model/AlbumRepository.java
@@ -1,10 +1,18 @@
 package com.northcoders.recordshopapp.model;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.northcoders.recordshopapp.service.AlbumApiService;
+import com.northcoders.recordshopapp.service.RetrofitInstance;
+
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AlbumRepository {
     private MutableLiveData<List<Album>> mutableLiveData = new MutableLiveData<>();
@@ -12,5 +20,26 @@ public class AlbumRepository {
 
     public AlbumRepository(Application application){
         this.application = application;
+    }
+
+    public MutableLiveData<List<Album>> getMutableLiveData(){
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<List<Album>> call = albumApiService.getAllAlbums();
+
+        call.enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> responseValue = response.body();
+
+                getMutableLiveData().setValue(responseValue);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable throwable) {
+                Log.i("HTTP Failure", throwable.getMessage());
+            }
+        });
+
+        return mutableLiveData;
     }
 }
