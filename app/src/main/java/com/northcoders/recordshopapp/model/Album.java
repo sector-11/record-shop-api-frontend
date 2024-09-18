d3e1bcd29a888d99983298fc3d9ec78c7622afe9
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-18 22:26:16 +0100

    feat: add data binding annotations and make album observable

--- app/src/main/java/com/northcoders/recordshopapp/model/Album.java
+++ app/src/main/java/com/northcoders/recordshopapp/model/Album.java
@@ -1,8 +1,12 @@
 package com.northcoders.recordshopapp.model;
 
-import com.google.gson.annotations.SerializedName;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 
-public class Album {
+import com.google.gson.annotations.SerializedName;
+import com.northcoders.recordshopapp.BR;
+
+public class Album extends BaseObservable {
     @SerializedName("id")
     private int id;
     @SerializedName("albumName")
@@ -25,43 +29,53 @@ public class Album {
     public Album(){
     }
 
+    @Bindable
     public String getId() {
         return String.valueOf(id);
     }
 
     public void setId(int id) {
         this.id = id;
+        notifyPropertyChanged(BR.id);
     }
 
+    @Bindable
     public String getAlbumName() {
         return albumName;
     }
 
     public void setAlbumName(String albumName) {
         this.albumName = albumName;
+        notifyPropertyChanged(BR.albumName);
     }
 
+    @Bindable
     public String getArtist() {
         return artist;
     }
 
     public void setArtist(String artist) {
         this.artist = artist;
+        notifyPropertyChanged(BR.artist);
     }
 
+    @Bindable
     public String getReleaseYear() {
         return String.valueOf(releaseYear);
     }
 
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
+        notifyPropertyChanged(BR.releaseYear);
     }
 
+    @Bindable
     public String getGenre() {
         return genre;
     }
 
     public void setGenre(String genre) {
         this.genre = genre;
+        notifyPropertyChanged(BR.genre);
     }
 }
