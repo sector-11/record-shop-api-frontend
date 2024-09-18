3f4b47c7881ea2db61f4a9d46728a40aedab68dc
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-18 22:14:44 +0100

    feat: make fields of album private and add getter/setter methods

--- app/src/main/java/com/northcoders/recordshopapp/model/Album.java
+++ app/src/main/java/com/northcoders/recordshopapp/model/Album.java
@@ -4,15 +4,15 @@ import com.google.gson.annotations.SerializedName;
 
 public class Album {
     @SerializedName("id")
-    int id;
+    private int id;
     @SerializedName("albumName")
-    String albumName;
+    private String albumName;
     @SerializedName("artist")
-    String artist;
+    private String artist;
     @SerializedName("releaseYear")
-    int releaseYear;
+    private int releaseYear;
     @SerializedName("genre")
-    String genre;
+    private String genre;
 
     public Album(int id, String albumName, String artist, int releaseYear, String genre) {
         this.id = id;
@@ -23,5 +23,45 @@ public class Album {
     }
 
     public Album(){
+    }
+
+    public String getId() {
+        return String.valueOf(id);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseYear() {
+        return String.valueOf(releaseYear);
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 }
