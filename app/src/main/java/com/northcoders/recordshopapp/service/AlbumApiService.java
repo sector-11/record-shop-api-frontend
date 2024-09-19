c755c30c1bae7dd54e544fe5d524ed6c11f25472
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-19 11:01:24 +0100

    feat: add post method to apiservice interface

--- app/src/main/java/com/northcoders/recordshopapp/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshopapp/service/AlbumApiService.java
@@ -5,9 +5,14 @@ import com.northcoders.recordshopapp.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
     @GET("records")
     public Call<List<Album>> getAllAlbums();
+
+    @POST("records")
+    public Call<Album> postAlbum(@Body Album album);
 }
